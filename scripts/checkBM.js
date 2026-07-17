b94f541bc4a4d335781b64d716ddae359a6c544a
matheussantos-web <matheus281197@gmail.com>
2026-07-17 15:29:45 -0300

    Remove test script

--- scripts/checkBM.js
+++
@@ -1,9 +0,0 @@
-const db = require('../src/db/init').getDb();
-const locations = db.prepare('SELECT id, name FROM locations').all();
-const bmCount = db.prepare("SELECT COUNT(*) as c FROM market_prices WHERE location_id = (SELECT id FROM locations WHERE name = 'Black Market')").get();
-const total = db.prepare('SELECT COUNT(*) as c FROM market_prices').get();
-const cityCounts = db.prepare('SELECT l.name, COUNT(*) as c FROM market_prices mp JOIN locations l ON l.id = mp.location_id GROUP BY l.name').all();
-console.log('locations:', locations);
-console.log('black market records:', bmCount);
-console.log('total records:', total);
-console.log('by city:', cityCounts);
