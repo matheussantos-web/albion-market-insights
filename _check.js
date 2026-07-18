56d69d46d4dd88923adce679f9b5c43f02b4cafc
matheussantos-web <matheus281197@gmail.com>
2026-07-18 00:52:45 -0300

    remove temp _check.js

--- _check.js
+++
@@ -1,7 +0,0 @@
-const db = require('./src/db/init').getDb();
-const fks = db.pragma('foreign_key_list(market_prices)');
-console.log('FKs:', JSON.stringify(fks));
-const cols = db.pragma('table_info(market_prices)');
-console.log('Columns:', cols.map(c => c.name).join(', '));
-const count = db.prepare('SELECT COUNT(*) as n FROM market_prices').get();
-console.log('Rows:', count.n);
